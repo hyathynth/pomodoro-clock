f0ffad7c3b35e3a0f0471405112c9fb75d7ba0f9
Qin Chen <hyathynth@gmail.com>
2017-09-20 23:11:06 -0400

    correct hr var using jslint

--- Pomodoro.js
+++ Pomodoro.js
@@ -12,7 +12,7 @@ function addTime (ele) {
 
 function formatTime (hr, min, sec) {
     if (hr < 10) {
-        has_hr = ('0' + hr).slice(-2);
+        hr = ('0' + hr).slice(-2);
     }
     if (min < 10) {
         min = ('0' + min).slice(-2);
@@ -23,7 +23,7 @@ function formatTime (hr, min, sec) {
     if (hr == 0) {
         return min + ':' + sec;
     }
-    else return has_hr + ':' + min + ':' + sec;
+    else return hr + ':' + min + ':' + sec;
 }
 
 function displayTime (session, ele, optional){
@@ -32,7 +32,7 @@ function displayTime (session, ele, optional){
     var temp_min = Math.floor((temp_time / 60 - temp_hr * 60));
     var temp_sec = Math.floor(temp_time - temp_hr*3600 - temp_min*60);
     ele.removeClass();
-   
+
     if (optional === undefined) {}
     if (optional == 1) {
         if (temp_hr == 0 && temp_min > 0 && temp_min <= 2 && temp_sec <= 59) {
@@ -41,7 +41,7 @@ function displayTime (session, ele, optional){
         else if (temp_hr == 0 && temp_min == 0 && temp_sec <= 59) {
         ele.addClass("red");
     }
-        else ele.addClass("green"); 
+        else ele.addClass("green");
     }
      ele.text(formatTime(temp_hr, temp_min, temp_sec));
 }
@@ -58,7 +58,7 @@ function updateTime (ele) {
         temp_min = str_time[1];
         temp_sec = str_time[2];
     }
-    
+
     if (temp_sec > 0) {
         temp_sec--;
     }
@@ -70,7 +70,7 @@ function updateTime (ele) {
         temp_min = 59;
         temp_hr--;
     }
-    
+
     if (temp_hr == 0 && temp_min > 0 && temp_min <= 2 && temp_sec <= 59) {
         ele.addClass("yello");
     }
@@ -78,7 +78,7 @@ function updateTime (ele) {
         ele.addClass("red");
     }
     else ele.addClass("green");
-        
+
     ele.text(formatTime(temp_hr, temp_min, temp_sec));
 }
 
@@ -93,39 +93,39 @@ $(document).ready(function () {
     var break_count = $('#break').text() * 60;
     var temp_count = session_count;
     var name_count = 0;
-    
+
     displayTime($('#session').text(), $('#clock'));
-    
-    
+
+
     $('#s_minus').on('click', function(){
         subsTime($('#session'));
         session_count = $('#session').text() * 60;
         temp_count = session_count;
         displayTime($('#session').text(), $('#clock'));
     })
-      
-    
+
+
     $('#s_add').on('click', function(){
-        addTime($('#session')); 
+        addTime($('#session'));
         session_count = $('#session').text() * 60;
         temp_count = session_count;
         displayTime($('#session').text(), $('#clock'));
     })
-    
-    
+
+
      $('#b_minus').on('click', function(){
         subsTime($('#break'));
         break_count = $('#break').text() * 60;
-        
+
     })
-    
+
      $('#b_add').on('click', function(){
         addTime($('#break'));
         break_count = $('#break').text() * 60;
-        
+
     })
-    
-   
+
+
     $('#play').on('click', function() {
         $(this).prop("disabled", true);
         $("#pause").removeClass("enter");
@@ -141,13 +141,13 @@ $(document).ready(function () {
 //            console.log(temp_count);
 //            console.log("s" + session_count);
 //            console.log("b" + break_count);
-            
+
             if (temp_count < 0) {
-                displayTime(break_count/60, $('#clock'), 1); 
+                displayTime(break_count/60, $('#clock'), 1);
                 temp_count = break_count;
                 break_count = session_count;
-                session_count = temp_count; 
-                
+                session_count = temp_count;
+
                 if (name_count%2 == 0) {
                     $('#clock_name').text("Break");
                 }
@@ -161,9 +161,9 @@ $(document).ready(function () {
         $(this).prop("disabled", true);
         $("#play").removeClass("enter");
         $('#play').prop("disabled", false);
-        clearInterval(interval); 
+        clearInterval(interval);
     })
-    
+
     $('#stop').on('click', function(){
         name_count = 0;
         $('#play').prop("disabled", false);
@@ -173,14 +173,14 @@ $(document).ready(function () {
         $("#s_add").prop("disabled", false);
         $("#b_minus").prop("disabled", false);
         $("#b_add").prop("disabled", false);
-        clearInterval(interval); 
+        clearInterval(interval);
         session_count = $('#session').text() * 60;
         break_count = $('#break').text() * 60;
         temp_count = session_count;
         $('#clock_name').text("Session");
         displayTime($('#session').text(), $('#clock'));
     })
-    
+
     $('#reset').on('click', function(){
         name_count = 0;
         $('#play').prop("disabled", false);
@@ -199,71 +199,70 @@ $(document).ready(function () {
         $('#clock_name').text("Session");
         displayTime($('#session').text(), $('#clock'));
     })
-    
+
     $('#s_add').on('mouseenter', function(){
         $(this).addClass("enter");
     })
-    
+
     $('#s_add').on('mouseleave', function(){
         $(this).removeClass("enter");
     })
-    
+
     $('#b_add').on('mouseenter', function(){
         $(this).addClass("enter");
     })
-    
+
     $('#b_add').on('mouseleave', function(){
         $(this).removeClass("enter");
     })
-    
+
     $('#s_minus').on('mouseenter', function(){
         $(this).addClass("enter");
     })
-    
+
     $('#s_minus').on('mouseleave', function(){
         $(this).removeClass("enter");
     })
-    
+
     $('#b_minus').on('mouseenter', function(){
         $(this).addClass("enter");
     })
-    
+
     $('#b_minus').on('mouseleave', function(){
         $(this).removeClass("enter");
     })
-    
+
     $('#play').on('mouseenter', function(){
         $(this).addClass("enter");
     })
-    
+
     $('#play').on('mouseleave', function(){
         $(this).removeClass("enter");
     })
-    
+
     $('#pause').on('mouseenter', function(){
         $(this).addClass("enter");
     })
-    
+
     $('#pause').on('mouseleave', function(){
         $(this).removeClass("enter");
     })
-    
+
     $('#reset').on('mouseenter', function(){
         $(this).addClass("enter");
     })
-    
+
     $('#reset').on('mouseleave', function(){
         $(this).removeClass("enter");
     })
-    
+
     $('#stop').on('mouseenter', function(){
         $(this).addClass("enter");
     })
-    
+
     $('#stop').on('mouseleave', function(){
         $(this).removeClass("enter");
     })
-    
-    
+
+
 })
-    
